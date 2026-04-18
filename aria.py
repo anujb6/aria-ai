3fdaa543dad8d7d01465c7c708f1ea28492b7a1c
anuj <74948201+anujb6@users.noreply.github.com>
2026-04-18 11:58:55 +0530

    Update aria.py

--- aria.py
+++ aria.py
@@ -14,6 +14,7 @@ import json
 import requests
 import datetime
 
+#this is speech agent
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
